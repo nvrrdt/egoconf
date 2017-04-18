b230453477794caa9d9260adcce424f08367c58c
nvrrdt <nicoverrijdt@gmail.com>
2017-04-18 20:26:49 +0200

    Add a link on EgoHome directing to Signup

--- src/components/EgoHome.vue
+++ src/components/EgoHome.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="base">
+    <a class="text-capitalize" v-on:click="toSignup" role="button" href>signup</a>
     <h2>{{ msg }}</h2>
     <p>test</p>
   </div>
 </template>
 
 <script>
+import router from '../router'
+
 export default {
   name: 'base',
   data () {
     return {
       msg: 'OK'
+    }
+  },
+  methods: {
+    toSignup: function () {
+      router.push({ name: 'AccountSignup' })
     }
   }
 }
@@ -32,7 +40,20 @@ li {
   margin: 0 10px;
 }
 
-a {
-  color: #42b983;
+/* unvisited link grey */
+#base a:link {
+	color: #777;
+}
+/* visited link grey */
+#base a:visited {
+	color: #777;
+}
+/* mouse over link blue */
+#base a:hover {
+	color: #0CF;
+}
+/* selected link blue */
+#base a:active {
+	color: #0CF;
 }
 </style>
---
+++ src/components/account/Signup.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="base">
+    <h2>{{ msg }}</h2>
+    <p>test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'base',
+  data () {
+    return {
+      msg: 'SignupView'
+    }
+  },
+  methods: {
+    handleSignUp: function () { /*
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END createwithemail] */
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
