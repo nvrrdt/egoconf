f7290bd42b52b65ff696b6a92e244031375e10de
nvrrdt <nicoverrijdt@gmail.com>
2017-05-27 18:57:22 +0200

    Eslint complained so imports are now brought together

--- src/main.js
+++ src/main.js
@@ -1,26 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import './firebase'
-
 import Vue from 'vue'
 import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
-
 import BootstrapVue from 'bootstrap-vue'
-Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon.vue'
-Vue.component('icon', Icon)
-
 import AuthPlugin from '@/auth'
 import * as firebase from 'firebase'
 
+Vue.component('icon', Icon)
+
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
 Vue.use(VueFire)
 Vue.use(AuthPlugin)
 
