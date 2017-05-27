d47310e50aac0351bf997e9cc54f7cdaedd8ed91
nvrrdt <nicoverrijdt@gmail.com>
2017-05-27 17:57:22 +0200

    Keep some old code as a reminder

--- src/components/base/Messages.vue
+++ src/components/base/Messages.vue
@@ -1,12 +1,49 @@
 <template>
-  <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-    <a class="btn btn-secondary text-capitalize active" href="/base/messages" role="button">Messages</a>
-    <a class="btn btn-secondary text-capitalize" href="/base/profile" role="button">Profile</a>
-    <a class="btn btn-secondary text-capitalize" href="/base/explore" role="button">Explore</a>
-    <a class="btn btn-secondary text-capitalize" href="/base/history" role="button">History</a>
+  <div>
+    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+      <a class="btn btn-secondary text-capitalize active" href="/base/messages" role="button">Messages</a>
+      <a class="btn btn-secondary text-capitalize" href="/base/profile" role="button">Profile</a>
+      <a class="btn btn-secondary text-capitalize" href="/base/explore" role="button">Explore</a>
+      <a class="btn btn-secondary text-capitalize" href="/base/history" role="button">History</a>
+    </div>
+    <div>
+      <div>
+          <input v-model="user" placeholder="Add a user"/>
+          <button @click="addItem">Add item</button>
+      </div>
+      <div>
+          <ul>
+              <li v-for="user of users">{{user.name}}</li>
+          </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import {db} from '@/firebase'
+
+export default {
+  data: () => ({
+    users: {}
+  }),
+
+  firebase: {
+    users: {
+      source: db.ref('users'),
+      // Optional, allows you to handle any errors.
+      cancelCallback (err) {
+        console.error(err)
+      }
+    }
+  },
+  methods: {
+    addItem () {
+      this.$firebaseRefs.users.push({
+        name: this.user
+      })
+    }
+  }
+}
 </script>
 <style>
   .btn{
