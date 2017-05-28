06f4e71c2d747f3c2fb9addbc69efdd149f8ae10
nvrrdt <nicoverrijdt@gmail.com>
2017-05-28 16:58:49 +0200

    First addings to explore page

--- src/components/base/Profile.vue
+++ src/components/base/Profile.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-    <a class="btn btn-secondary text-capitalize" href="/base/messages" role="button">Messages</a>
-    <a class="btn btn-secondary text-capitalize active" href="/base/profile" role="button">Profile</a>
-    <a class="btn btn-secondary text-capitalize" href="/base/explore" role="button">Explore</a>
-    <a class="btn btn-secondary text-capitalize" href="/base/history" role="button">History</a>
+  <div>
+    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+      <a class="btn btn-secondary text-capitalize" href="/base/messages" role="button">Messages</a>
+      <a class="btn btn-secondary text-capitalize active" href="/base/profile" role="button">Profile</a>
+      <a class="btn btn-secondary text-capitalize" href="/base/explore" role="button">Explore</a>
+      <a class="btn btn-secondary text-capitalize" href="/base/history" role="button">History</a>
+    </div>
+    <div>
+      <p>{{ coordinates['firstname'] }}</p>
+      <p>{{ coordinates.lastname }}</p>
+      <p>{{ coordinates.handle }}</p>
+    </div>
   </div>
 </template>
 <script>
+import * as firebase from 'firebase'
+
+export default {
+  data () {
+    return {
+      coordinates: {}
+    }
+  },
+  created () {
+    this.getCoordinates()
+  },
+  methods: {
+    getCoordinates: function () {
+      var vm = this
+
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+          var userId = user.uid
+
+          firebase.database().ref('/users/' + userId).once('value').then(function (snapshot) {
+            vm.coordinates = snapshot.val()
+          })
+
+          return vm.coordinates
+        } else {
+          return 'hemel'
+        }
+      })
+    }
+  }
+}
 </script>
 <style>
   .btn{
