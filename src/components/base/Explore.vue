f794bd86666c491f00033fc67a990e55a9f81c3c
nvrrdt <nicoverrijdt@gmail.com>
2017-05-28 16:59:58 +0200

    Preparing modal for further development

--- src/components/base/Explore.vue
+++ src/components/base/Explore.vue
@@ -5,28 +5,43 @@
       <a class="btn btn-secondary text-capitalize" href="/base/profile" role="button">Profile</a>
       <a class="btn btn-secondary text-capitalize active" href="/base/explore" role="button">Explore</a>
       <a class="btn btn-secondary text-capitalize" href="/base/history" role="button">History</a>
-    </div>     
-    <div id="wrapper" class="container"> 
+    </div>
+    <div>
+        <h3>Userprofile from:</h3>
+        <!-- <p>Name: {{ view.first_name }} {{ view.last_name }}</p>
+        <p>Handle: {{ view.handle }}</p> -->
+    </div> 
+    <div id="wrapper" class="container" v-on:keyup.esc="closeModal"> 
       <modal v-if="showModal"> 
-        <h3 slot="header" class="modal-title">
-          Modal title
+        <h3 slot="header" class="modal-title pull-right">
+          <button type="button" class="close pull-right" data-dismiss="modal" @click="closeModal()">&times;</button>
+          <h4>Grade a quality</h4>
         </h3>
         
         <div slot="body">
           test3
+          <!-- help_texts = {
+            'quality': ('Insert a the quality you like to grade'),
+            'project': ('Insert the project where you got to know this person\'s quality'),
+            'grade': ('Give a number between 6 and 10 on a scale to 10'),
+          }
+          if data < 6:
+            raise ValidationError("Value must be greater than or equal to 6")
+          elif data > 10:
+            raise ValidationError("Value must be lessser than or equal to 10") -->
         </div>
 
         <div slot="footer">
-          <button type="button" class="btn btn-outline-info" @click="closeModal()">Close</button>
           <button type="button" class="btn btn-primary" data-dismiss="modal" @click="submitAndClose()">Submit</button>
         </div>
       </modal>
-      <button type="button" class="btn btn-primary" @click="openModal()">Open Modal</button>
+      <button type="button" class="btn btn-primary" @click="openModal()">Grade a quality</button>
     </div>
   </div>
 </template>
 <script>
-import Modal from '@/components/base/ExploreModal'
+// taken from JuneRockwell/BootstrapVueModal
+import Modal from '@/components/Modal'
 
 export default {
   components: {
@@ -58,5 +73,8 @@ export default {
   }
   #wrapper {
     margin-top: 60px;
+  }
+  .modal-title {
+    width: 100%;
   }
 </style>
