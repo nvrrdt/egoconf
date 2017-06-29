7a7d8bf39337cc6af4efb345278675ce89aaaefa
nvrrdt <nicoverrijdt@gmail.com>
2017-06-29 16:59:54 +0200

    Add the message list for the history

--- src/components/base/History.vue
+++ src/components/base/History.vue
@@ -6,12 +6,41 @@
       <a class="btn btn-secondary text-capitalize" href="/base/explore" role="button">Explore</a>
       <a class="btn btn-secondary text-capitalize active" href="/base/history" role="button">History</a>
     </div>
-    <div v-if="!isAuthenticated">Not logged in</div>
-    <div v-if="isAuthenticated">Logged in</div>
+    <div>
+      <ul style="margin-top: 35px;">
+        <li v-for="message in messages" :key="message.messagekey" v-if="message.messagevalue.is_accepted">
+          <div class="text-left message">
+            <p>{{ message.messagevalue.from_userid }} accepted your message, where you give {{ message.messagevalue.grade }} points for the '{{ message.messagevalue.quality }}' quality during the '{{ message.messagevalue.project }}' project</p>
+          </div>  
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import * as firebase from 'firebase'
+// import {db} from '@/firebase'
+import store from '@/store'
 
+export default {
+  data: () => ({
+    messages: store.getMessages(),
+    message: {}
+  }),
+  created: function () {
+    this.getMessages()
+  },
+  methods: {
+    getMessages () {
+      var userid = firebase.auth().currentUser.uid
+      var myMessagesRef = firebase.database().ref('messages').orderByChild('from_userid').equalTo(userid)
+      myMessagesRef.on('child_added', function (snapshot) {
+        var dict = { messagekey: snapshot.key, messagevalue: snapshot.val(), isCollapsed: false }
+        store.setMessages(dict)
+      })
+    }
+  }
+}
 </script>
 <style>
   .btn{
@@ -19,5 +48,9 @@
     margin-right:10px;
     margin-bottom:10px;
     margin-left:10px;
+  }
+  .message {
+    margin-top:5px;
+    margin-bottom:5px;
   }
 </style>
