29bf449a194a0096a738596ee33534eb3471b931
nvrrdt <nicoverrijdt@gmail.com>
2017-07-11 19:24:24 +0200

    Forget about constraints.js for now

--- src/constraints.js
+++
@@ -1,7 +0,0 @@
-/**
- * is_unknown || is_inappropriate = max 3 per user === blocked
- * 4 users where blocked === banned for 1) 1 week 2) 2 weeks 3) 1 month 4) 3 months 5) 6 months
- * >= 20 messages for more users per 15 minutes === bomb => block messages until over
- * max 1 unique quality being graded in 5 days to one to_user === pauze
- * max 5 graded different qualities per 5 days from one user to unique other
- */
