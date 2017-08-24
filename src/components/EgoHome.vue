15e2d65e7f08b87f9c989c3ea4a98fe245c70780
nvrrdt <nicoverrijdt@gmail.com>
2017-08-24 14:24:41 +0200

    Small text adaptation

--- src/components/EgoHome.vue
+++ src/components/EgoHome.vue
@@ -45,7 +45,7 @@
           to allow fulltime development and perhaps an expansion of manpower.
         </p>
         <p class="card-text text-left">
-          DISCLAIMER: egoconf, as is, is a demo, a proof-of-concept, and could only experimentally be used, 
+          DISCLAIMER: egoconf, as is, is a demo (or a proof-of-concept or a prototype) and could only experimentally be used, 
           which means that the gathered data will likely be deleted as needed.
         </p>
       </div>
