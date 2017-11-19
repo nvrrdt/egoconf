7300a8e5be1957d590c5afbfc738c302b08eee57
nvrrdt <nicoverrijdt@gmail.com>
2017-11-19 19:58:21 +0100

    Functions part of sending mail to admin

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const nodemailer = require('nodemailer');
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+});
+
+// Sends an email to an egoconf admin.
+exports.contactmessage = functions.database.ref('/contactmessages/{msgId}').onCreate(event => {
+  const snapshot = event.data;
+  const val = snapshot.val();
+
+  const mailOptions = {
+    from: '"egoconf contact form" <' + gmailEmail + '>',
+    to: gmailEmail
+  };
+
+  mailOptions.subject = 'Mail from ' + val.email;
+  mailOptions.text = val.fulluser + '\n' + val.msg;
+
+  const msgid = event.params.msgId;
+  var msgRef = admin.database().ref('/contactmessages/' + msgid)
+  msgRef.remove()
+
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New email sent to admin from ', val.email, val.fulluser);
+  }).catch(error => {
+    console.error('There was an error while sending the email: ', error);  
+  });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
